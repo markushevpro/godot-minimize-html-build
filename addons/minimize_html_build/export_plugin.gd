e48e4f02f794e6bde12b020de2b19f615dc9925a
Artem Markushev <artem@markushev.pro>
2025-12-22 15:52:27 +0300

    Fixed running on not-Web templates

--- addons/minimize_html_build/export_plugin.gd
+++ addons/minimize_html_build/export_plugin.gd
@@ -3,7 +3,7 @@ extends EditorExportPlugin
 
 var _plugin_name = "Minimize HTML Build"
 var _export_info: MHEPExportInfo
-
+var _runned: bool = false
 
 func _get_name() -> String:
 	return _plugin_name
@@ -14,22 +14,27 @@ func _supports_platform( platform: EditorExportPlatform ) -> bool:
 
 
 func _export_begin( 
-		_features: PackedStringArray, 
+		features: PackedStringArray, 
 		is_debug: bool, 
 		path: String, 
 		_flags: int 
 ):
-	MHEPUtils.enable_debug( is_debug )
-	MHEPUtils.debug( "", "---- EXPORT STARTED ----" )
+	_runned = false
 	
-	_export_info = MHEPExportInfo.new( path )
+	if features.has("web"):
+		_runned = true
+		MHEPUtils.enable_debug( is_debug )
+		MHEPUtils.debug( "", "---- EXPORT STARTED ----" )
+		
+		_export_info = MHEPExportInfo.new( path )
 
 
 func _export_end():
-	# Run compression after all project files were created
-	var copier = MHEPCopier.new( _export_info )
-	
-	MHEPCompresser.compress( copier )
-	MHEPHTML.fix( _export_info )
-	MHEPJS.fix( _export_info )
-	MHEPUtils.debug( "", "---- EXPORT FINISHED ----" )
+	if _runned:
+		# Run compression after all project files were created
+		var copier = MHEPCopier.new( _export_info )
+		
+		MHEPCompresser.compress( copier )
+		MHEPHTML.fix( _export_info )
+		MHEPJS.fix( _export_info )
+		MHEPUtils.debug( "", "---- EXPORT FINISHED ----" )
